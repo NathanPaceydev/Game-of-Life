fa04fe45cb7b1230108363a056685d37ee5ec56f
nathanpaceydev <npacey01@gmail.com>
2022-04-23 15:02:24 -0400

    ver 2.1

--- game_of_life.py
+++ game_of_life.py
@@ -26,6 +26,30 @@ MARGIN = 5
 
 WINDOW_SIZE = [255*SIZE/10,300*SIZE/10]
 
+screen = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Game of Life")
+
+arrow_image = pygame.image.load("start_btn.png").convert_alpha()
+arrow_image = pygame.transform.scale(arrow_image,((WINDOW_SIZE[0]/5,WINDOW_SIZE[0]/6)))
+
+exit_image = pygame.image.load("exit_btn.png").convert_alpha()
+exit_image = pygame.transform.scale(exit_image,((WINDOW_SIZE[0]/5,WINDOW_SIZE[0]/6)))
+
+
+
+class Button():
+    def __init__(self,x,y,image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+
+    def draw(self):
+        screen.blit(self.image, (self.rect.x,self.rect.y))
+    
+start_button = Button(WINDOW_SIZE[0]/2-WINDOW_SIZE[0]/3,WINDOW_SIZE[1]-WINDOW_SIZE[0]/6, arrow_image)
+exit_button = Button(WINDOW_SIZE[0]-WINDOW_SIZE[0]/3,WINDOW_SIZE[1]-WINDOW_SIZE[0]/6,exit_image)
+
+
 
 def gridSetUp(size):
     return [[0]*size for _ in range(size)]
@@ -34,10 +58,6 @@ def main():
     grid = gridSetUp(SIZE)
     pygame.init()
     try:
-        screen = pygame.display.set_mode(WINDOW_SIZE)
-
-        pygame.display.set_caption("Game of Life")
-
         clock = pygame.time.Clock()
 
         runFlag = True
@@ -79,6 +99,9 @@ def main():
                               (MARGIN + HEIGHT) * row + MARGIN,
                               WIDTH,
                               HEIGHT])
+
+                    start_button.draw()
+                    exit_button.draw()
 
             clock.tick(60)
 
