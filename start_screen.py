8d08e062d4aae8c613a06fbb2d338b1557160b8e
nathanpaceydev <npacey01@gmail.com>
2022-05-05 14:09:32 -0400

    ver 2.6.2 start screen

--- start_screen.py
+++ start_screen.py
@@ -4,71 +4,70 @@ import sys
 def start():
     count = 0
     pygame.init()
-
+  
     clock = pygame.time.Clock()
-
+    
     # it will display on screen
     screen = pygame.display.set_mode([600, 500])
-
+    pygame.display.set_caption("Game of Life")
     # basic font for user typed
     base_font = pygame.font.Font(None, 32)
     user_text = ''
-
+    
     # create rectangle
     input_rect = pygame.Rect(200, 200, 140, 32)
+    
     # color_active stores color(lightskyblue3) which
     # gets active when input box is clicked by user
     color_active = pygame.Color('lightskyblue3')
-
+    
     # color_passive store color(chartreuse4) which is
     # color of input box.
     color_passive = pygame.Color('chartreuse4')
     color = color_passive
-
+    
     active = False
-
+    
     while True:
         for event in pygame.event.get():
-
+    
         # if user types QUIT then the screen will close
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
+    
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if input_rect.collidepoint(event.pos):
                     active = True
                 else:
                     active = False
-
+    
             if event.type == pygame.KEYDOWN:
-
+    
                 # Check for backspace
                 if event.key == pygame.K_BACKSPACE:
-
                     # get text input from 0 to -1 i.e. end.
                     user_text = user_text[:-1]
-
-                if(event.key == pygame.K_RETURN):
-                    if count==0:
-                        width_text = user_text
-                        print("WIDTH",width_text)
-                    if count ==1:
-                        height_text = user_text
-                        print("HEIGHT",height_text)
-                        print("start game")
-                        return width_text, height_text
-                    count +=1
-                    user_text = ""
-
+                    
                 # Unicode standard is used for string
                 # formation
                 else:
                     user_text += event.unicode
+
+                if event.key == pygame.K_RETURN:
+                    if count == 0:
+                        width = user_text
+                        user_text = ''
+                    if count == 1:
+                        length = user_text
+                        user_text = ''
+                        return width,length
+                    count+=1
+               
         
         # it will set background color of screen
         screen.fill((255, 255, 255))
-
+    
         if active:
             color = color_active
         else:
@@ -77,7 +76,7 @@ def start():
         # draw rectangle and argument passed which should
         # be on screen
         pygame.draw.rect(screen, color, input_rect)
-
+    
         text_surface = base_font.render(user_text, True, (255, 255, 255))
         
         # render at position stated in arguments
